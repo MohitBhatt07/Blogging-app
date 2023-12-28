1878809fc6b66dad583b41f1d7b51e81bda9bc1f
Mohit Bhatt <bhattmohit869@gmail.com>
2023-12-28 20:54:33 +0530

    added cover image option

--- models/blog.js
+++ models/blog.js
@@ -5,6 +5,10 @@ const blogSchema = new Schema({
     type : String,
     required : true
   },
+  content : { 
+    type : String,
+    required  : true,
+  },
   coverImageUrl :{
     type : String,
     required : false,
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,30 @@
 const express = require('express');
-
+const { blog } = require('../models/blog');
+const multer = require('multer');
 const router = express.Router();
+const path = require('path');
 
-router.get('/add-new',(req, res) => {
-  res.render('addBlog', {user: req.user})
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, `./public/uploads/${req.user._id}`);
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  }
+})
+const upload = multer({ storage: storage });
+
+router.get('/addnew',(req, res) => {
+  res.render('addBlog', {user: req.user});
 });
 
+router.post('/addnew',upload.single('coverImage'),async (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.redirect("/");
+  // const {title , content ,coverImage}  = req.body;
+  // const newBlog = await blog.create({title: title, content: content });
+  // return res.json({ "blog" : newBlog ,"msg" :"blog is created"});
+});
 module.exports = router;
