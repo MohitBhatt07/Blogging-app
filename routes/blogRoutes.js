86ad182a0530c70f0e6c6af5a4f5be32f744d735
Mohit Bhatt <bhattmohit869@gmail.com>
2024-01-02 21:00:13 +0530

    created comment section

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
 const router = express.Router();
 const path = require("path");
-const User = require("../models/user");
+const Comment = require("../models/comment");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -21,17 +21,33 @@ router.get("/addnew", (req, res) => {
   res.render("addBlog", { user: req.user });
 });
 
-router.get('/:id' , async(req,res)=>{
-  const findBlog =  await blog.findById(req.params.id);
-  const blogCreator = await User.findById(findBlog.createdBy);
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const blogDate =  findBlog.createdAt.toLocaleDateString('en-US', options).toUpperCase();
-  return res.render('blog' ,{ blog  :findBlog, blogDate : blogDate,blogCreator : blogCreator,user : req.user} );
+router.get("/:id", async (req, res) => {
+  const findBlog = await blog.findById(req.params.id).populate("createdBy");
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const blogDate = findBlog.createdAt
+    .toLocaleDateString("en-US", options)
+    .toUpperCase();
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+  console.log(comments);
+  return res.render("blog", {
+    blog: findBlog,
+    blogDate: blogDate,
+    blogCreator: findBlog.createdBy,
+    user: req.user,
+    comments: comments,
+  });
 });
 
 router.post("/addnew", upload.single("coverImage"), async (req, res) => {
-  const {title,content}= req.body;
-
+  const { title, content } = req.body;
   const newBlog = await blog.create({
     title,
     content,
@@ -40,5 +56,16 @@ router.post("/addnew", upload.single("coverImage"), async (req, res) => {
   });
   return res.redirect(`/blog/${newBlog._id}`);
   // return res.json({ "blog" : newBlog ,"msg" :"blog is created"});
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  const { content } = req.body;
+  const comment = await Comment.create({
+    content: content,
+    createdBy: req.user._id,
+    blogId: req.params.blogId,
+  });
+
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 module.exports = router;
